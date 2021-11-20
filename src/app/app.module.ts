7a8797e240387713bb988cd0779043c7e37c8fbf
Irene Martin <79365505+IMartinMenendez@users.noreply.github.com>
2021-11-20 12:29:32 +0000

    Initial commit with structure and initial front end prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main/header/header.component';
+import { SliderComponent } from './main/slider/slider.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardEventComponent } from './main/card-event/card-event.component';
+import { WelcomeComponent } from './main/welcome/welcome.component';
+import { NavBarComponent } from './main/nav-bar/nav-bar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    CardEventComponent,
+    WelcomeComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
